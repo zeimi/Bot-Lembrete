d6fe58b33fe096beeb295dd425f2df8be8caf583
Zeimi <contato.brunodurao@gmail.com>
2022-06-04 16:43:33 -0300

    Verificação hoje

--- funcoes.py
+++ funcoes.py
@@ -16,42 +16,51 @@ def verificacao(hora:str, data:str=""):
 
     # Verificação horário válido
     if int(verificaHora[0]) > 23 or int(verificaHora[1]) > 59:
-        print("Tá errado fi")
+        horavalida = False
     else:
-        print("Pode pá")
+        horavalida = True
 
     # Verificação Data Válida
     listaMes31 = [1, 3, 5, 7, 8, 10, 12]
     listaMes30 = [4, 6, 9, 11]
 
     if data == "":
-        None
+        datavalida = True
     else:
         if int(verificaData[1]) == 2:
             if isleap(anoEscolhido):
                 if int(verificaData[0]) <= 29:
-                    print("Pode pá")
+                    datavalida = True
+
                 else:
-                    print("Tá errado fi")
+                    datavalida = False
             else:
                 if int(verificaData[0]) <= 28:
-                    print("Pode pá")
+                    datavalida = True
                 else:
-                    print("Tá errado fi")
+                    datavalida = False
 
         else:
             for i in listaMes31:
                 if int(verificaData[1]) == i and int(verificaData[0]) <= 31:
-                    print("Pode pá")
+                    datavalida = True
+                    break
                 else:
-                    None
+                    datavalida = False
 
             for i in listaMes30:
                 if int(verificaData[1]) == i and int(verificaData[0]) <= 30:
-                    print("Pode pá")
+                    datavalida = True
+                    break
                 else:
-                    None
+                    datavalida = False
 
-    return None
+    if horavalida == True and datavalida == True:
+        return True
+    else:
+        return False
 
-verificacao(hora=horaEscolhida, data=dataEscolhida)
+if verificacao(hora=horaEscolhida, data=dataEscolhida) == True:
+    print("É verdade")
+elif verificacao(hora=horaEscolhida, data=dataEscolhida) == False:
+    print("Né nada")
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import datetime
 from datetime import *
 import asyncio
 from chaveBOT import chaveBOT
+from funcoes import verificacao
 
-bot = commands.Bot(description='Um bot feito para um trabalho universitário', command_prefix='.')
+bot = commands.Bot(
+    description='Um bot feito para um trabalho universitário', command_prefix='.')
 
 
 @bot.event
@@ -27,47 +29,65 @@ Registro de ações no console:
 """)
 
 
-@bot.event # Eventos de erro (Comando não encontrado, sem requerimentos ou requerimento errado)
+# Eventos de erro (Comando não encontrado, sem requerimentos ou requerimento errado)
+@bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound): # Comando não encontrado
+    if isinstance(error, commands.CommandNotFound):  # Comando não encontrado
         await ctx.send("Este comando não existe.")
 
-    if isinstance(error, commands.MissingRequiredArgument): # Faltou argumento (somar a + b -> somar 1)
+    # Faltou argumento (somar a + b -> somar 1)
+    if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Faltam Argumentos. Digite .ajuda para saber como utilizar o comando corretamente!")
 
-    if isinstance(error, commands.BadArgument): # Argumento inválido (somar a + b -> somar 1 danilo)
+    # Argumento inválido (somar a + b -> somar 1 danilo)
+    if isinstance(error, commands.BadArgument):
         await ctx.send("Argumento inválido. Digite .ajuda para saber como utilizar o comando corretamente!")
 
 
-@bot.command() # Comando de teste para ver se o bot pode mandar mensagens no servidor
+@bot.command()  # Comando de teste para ver se o bot pode mandar mensagens no servidor
 async def teste(ctx):
     await ctx.message.add_reaction("✅")
     await ctx.send("Teste concluido!")
     print("Teste concluido.")
 
-global datahoje # Variável global que pode ser utilizada em varias funções assíncrionas (async def)
-datahoje = datetime.now() # Função da biblioteca datetime que pega a data e hora atual
+# Variável global que pode ser utilizada em varias funções assíncrionas (async def)
+global datahoje
+datahoje = datetime.now()  # Função da biblioteca datetime que pega a data e hora atual
 
-@bot.command() # Indica que a função a seguir é um comando seguindo a API discord.py
-async def lembretehoje(ctx, horario: str, *, descricao: str): 
 
-    mensagemBot = await ctx.send(f"O horário: '{horario}' e a descrição '{descricao}' foram armazenados")
-    await mensagemBot.pin() # Fixa a mensagem no servidor
+@bot.command()  # Indica que a função a seguir é um comando seguindo a API discord.py
+async def lembretehoje(ctx, horario: str, *, descricao: str = ""):
 
-    global horalembretehoje # Variável global que pode ser acessada em outras funções (def)
-    global descricaohoje    # Idem
-    descricaohoje = descricao # Recebe o parâmetro passado pelo usuário
-    datahoje = datetime.now() # Atualiza o horário e data atuais armazenado no bot
+    if verificacao(hora=horario) == True:
+        if descricao == "":
+            mensagemBot = await ctx.send(f"O horário: '{horario}' foi armazenado")
 
-    horalembretehoje = datetime.strptime(horario, "%H:%M") # Coleta a string passada pelo usuário (xx:xx) e transforma em um objeto datetime
-    horalembretehoje = horalembretehoje.replace(day=int(datahoje.day))
-    horalembretehoje = horalembretehoje.replace(month=int(datahoje.month))# Troca o dia, mês e ano pelos atuais armazenados no bot
-    horalembretehoje = horalembretehoje.replace(year=int(datahoje.year))
-    print(f"Horário armazenado por {ctx.author}: "+ str(horalembretehoje))
-    return horalembretehoje, descricaohoje # Atualiza as variáveis globalmente
+        else:
+            mensagemBot = await ctx.send(f"O horário: '{horario}' e a descrição: '{descricao}' foram armazenados")
+
+        await mensagemBot.pin()  # Fixa a mensagem no servidor
+
+        # Variável global que pode ser acessada em outras funções (def)
+        global horalembretehoje
+        global descricaohoje    # Idem
+        descricaohoje = descricao  # Recebe o parâmetro passado pelo usuário
+        datahoje = datetime.now()  # Atualiza o horário e data atuais armazenado no bot
+
+        # Coleta a string passada pelo usuário (xx:xx) e transforma em um objeto datetime
+        horalembretehoje = datetime.strptime(horario, "%H:%M")
+        horalembretehoje = horalembretehoje.replace(day=int(datahoje.day))
+        # Troca o dia, mês e ano pelos atuais armazenados no bot
+        horalembretehoje = horalembretehoje.replace(month=int(datahoje.month))
+        horalembretehoje = horalembretehoje.replace(year=int(datahoje.year))
+        print(f"Horário armazenado por {ctx.author}: " + str(horalembretehoje))
+        return horalembretehoje, descricaohoje  # Atualiza as variáveis globalmente
+
+    else:
+        await ctx.send(f"O horário: '{horario}' é inválido!")
+
 
 @bot.command()
-async def lembretedia(ctx, horario: str, dia: str, *, descricao: str): 
+async def lembretedia(ctx, horario: str, dia: str, *, descricao: str):
     await ctx.send(f"O horário '{horario}', o dia '{dia}' e a descrição '{descricao}' foram armazenados")
     objetivo = horario+" "+dia
     datahoje = datetime.now()
@@ -78,6 +98,7 @@ async def lembretedia(ctx, horario: str, dia: str, *, descricao: str):
     horalembretedia = horalembretedia.replace(year=int(datahoje.year))
     print(f"Data armazenada por {ctx.author}: {str(horalembretedia)}")
     return horalembretedia, descricaodia
+
 
 @bot.command()
 async def ativarlembrete(ctx, lembrete: str):
@@ -90,9 +111,13 @@ async def ativarlembrete(ctx, lembrete: str):
         else:
             await ctx.send("O lembrete foi ativado!")
             descricao = descricaohoje
-            print(f"Tempo até o lembrete: {deltatempo.total_seconds()} Segundos")
+            print(
+                f"Tempo até o lembrete: {deltatempo.total_seconds()} Segundos")
             await asyncio.sleep(deltatempo.total_seconds())
-            await ctx.send(f"Lembrete do dia: {descricao} @everyone")
+            if descricao == "":
+                await ctx.send(f"Lembrete do dia! @everyone")
+            else:
+                await ctx.send(f"Lembrete do dia: '{descricao}' @everyone")
 
     elif lembrete == "dia":
         deltatempo = horalembretedia-datahoje
@@ -101,33 +126,45 @@ async def ativarlembrete(ctx, lembrete: str):
         else:
             await ctx.send("O lembrete foi ativado!")
             descricao1 = descricaodia
-            print(f"Segundos até o lembrete programado: {deltatempo.total_seconds()}")
+            print(
+                f"Segundos até o lembrete programado: {deltatempo.total_seconds()}")
             await asyncio.sleep(deltatempo.total_seconds())
             await ctx.send(f"Lembrete! {descricao1} @everyone")
 
 
-@bot.command() # Comando de teste para ver se o bot pode mandar mensagens no servidor
+@bot.command()  # Comando de teste para ver se o bot pode mandar mensagens no servidor
 async def dataagora(ctx):
     datahoje = datetime.now()
     await ctx.send(str(datahoje.strftime("**Hora atual:** %H:%M.\n**Data atual:** %d/%m/%Y.")))
-    print(f'Horário atual exibido! {datahoje.strftime("Hora atual: %H:%M. Data atual: %d/%m/%Y.")}')
+    print(
+        f'Horário atual exibido! {datahoje.strftime("Hora atual: %H:%M. Data atual: %d/%m/%Y.")}')
 
 
 @bot.command()
 async def ajuda(ctx):
-    embed = discord.Embed(title="Aqui estão todos os comandos do bot atualmente:", color=0x000080)
-    embed.add_field(name="**ajuda**", value="Mostra estes comandos", inline=False)
-    embed.add_field(name="**teste**", value="Um comando de teste, verifica se o bot está funcional.", inline=False)
-    embed.add_field(name="**lembretehoje**", value="Salva o horário como o lembrete de hoje. Uso: **lembretehoje 23:59**", inline=False)
-    embed.add_field(name="**lembretedia**", value="Salva um horário e um dia específico para o lembrete. Uso correto: **.lembretedia 23:59 31/12**", inline=False)
-    embed.add_field(name="**ativarlembrete**", value="Ativa um dos horários salvos e o utiliza como lembrete. Uso correto: **.ativar lembrete hoje/dia**", inline=False)
-    embed.add_field(name="**sugestao**", value="E-mail para contato de sugestões para o bot.", inline=False)
-    embed.add_field(name="**fale**", value="O bot fala o que você quiser. Uso correto: **.fale** (**o que o bot falará**).", inline=False)
-    embed.add_field(name="**renomear**", value="Bota o nome que você quiser no bot. Uso correto: **.renomear** (**novo nome do bot**).", inline=False)
-    embed.add_field(name="**limpar**", value="Limpe o chat. Uso correto: **.limpar** (**quantidade**) [sem quantidade informada, serão apagadas 100 mensagens].", inline=False)
-    embed.add_field(name="**Autoria**", value="Bot feito por Bruno Durão Silva e Gustavo Santos Rocha", inline=False)
+    embed = discord.Embed(
+        title="Aqui estão todos os comandos do bot atualmente:", color=0x000080)
+    embed.add_field(name="**ajuda**",
+                    value="Mostra estes comandos", inline=False)
+    embed.add_field(
+        name="**teste**", value="Um comando de teste, verifica se o bot está funcional.", inline=False)
+    embed.add_field(name="**lembretehoje**",
+                    value="Salva o horário como o lembrete de hoje. Uso: **lembretehoje 23:59**", inline=False)
+    embed.add_field(name="**lembretedia**",
+                    value="Salva um horário e um dia específico para o lembrete. Uso correto: **.lembretedia 23:59 31/12**", inline=False)
+    embed.add_field(name="**ativarlembrete**",
+                    value="Ativa um dos horários salvos e o utiliza como lembrete. Uso correto: **.ativar lembrete hoje/dia**", inline=False)
+    embed.add_field(name="**sugestao**",
+                    value="E-mail para contato de sugestões para o bot.", inline=False)
+    embed.add_field(
+        name="**fale**", value="O bot fala o que você quiser. Uso correto: **.fale** (**o que o bot falará**).", inline=False)
+    embed.add_field(name="**renomear**",
+                    value="Bota o nome que você quiser no bot. Uso correto: **.renomear** (**novo nome do bot**).", inline=False)
+    embed.add_field(name="**limpar**",
+                    value="Limpe o chat. Uso correto: **.limpar** (**quantidade**) [sem quantidade informada, serão apagadas 100 mensagens].", inline=False)
+    embed.add_field(name="**Autoria**",
+                    value="Bot feito por Bruno Durão Silva e Gustavo Santos Rocha", inline=False)
     await ctx.send(embed=embed)
-
 
 
 @bot.command()
@@ -135,9 +172,11 @@ async def limpar(ctx, quantidade=100):
     await ctx.channel.purge(limit=quantidade + 1)
     print("O chat foi limpo por {}!".format(ctx.author))
 
+
 @bot.command()
 async def sugestao(ctx):
     await ctx.send("Sugestões são bem vindas! E-mail para contato: **contato.brunodurao@gmail.com** ou **k13gustavo@gmail.com**")
+
 
 @bot.command()
 async def fale(ctx, *, arg):
@@ -145,12 +184,13 @@ async def fale(ctx, *, arg):
     await ctx.send(arg)
     print("O bot falou '{}' no chat!".format(arg))
 
-@bot.command() #comando para renomear o bot, funcionou uma vez mas agora não quer funfar por algum motivo
+
+@bot.command()  # comando para renomear o bot, funcionou uma vez mas agora não quer funfar por algum motivo
 async def renomear(ctx, *, name: str):
     await bot.user.edit(username=name)
 
 chave = int(input(
-"""O bot pode iniciar?
+    """O bot pode iniciar?
 1 = sim
 0 = não
 resposta: """
